8a20a1673614d23ed49727d2909411e4a7f17b8e
Angela Julie Dobbs <angela@jwplayer.com>
2016-07-18 19:22:26 -0400

    ship placement now working correctly after removing pass by reference errors

--- Scripts/game.js
+++ Scripts/game.js
@@ -6,27 +6,27 @@ var ships = [
   {
     name: "Aircraft Carrier",
     size: 5,
-    boardPositions: null
+    id: 0
   },
   {
     name: "Battleship",
     size: 4,
-    boardPositions: null
+    id: 1
   },
   {
     name: "Destroyer",
     size: 3,
-    boardPositions: null
+    id: 2
   },
   {
     name: "Submarine",
     size: 3,
-    boardPositions: null
+    id: 3
   },
   {
     name: "Patrol Boat",
     size: 2,
-    boardPositions: null
+    id: 4
   },
 ]
 
@@ -48,6 +48,7 @@ var placeShip = function(player, ship) {
   var testCoord, testStep;
   while (!shipPlaced) {
     testCoord = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    console.log(testCoord);
     testStep = directions[Math.floor(Math.random() * 4)];
     if (checkValidPlacement(player.fleetBoard, testCoord, testStep, ship.size)) {
       shipPlaced = true;
@@ -58,18 +59,34 @@ var placeShip = function(player, ship) {
 };
 
 var addShipToBoard = function(player, coord, step, ship) {
-
+  var addCoord = coord.slice(0);
+  var newShip = {
+    id: ship.id,
+    boardPositions: [coord]
+  };
+  player.ships.push(newShip);
+  player.fleetBoard[addCoord[0]][addCoord[1]].ship = newShip;
+  for (var jdx = 1; jdx < ship.size - 1; jdx++) {
+    addCoord[0] += step[0];
+    addCoord[1] += step[1];
+    newShip.boardPositions.push(addCoord.slice(0))
+    player.fleetBoard[addCoord[0]][addCoord[1]].ship = newShip;
+  }
 }
 
 var checkValidPlacement = function(board, coord, step, size) {
   // var flag = true;
-  var checkCoord;
-  for (var check = 0; check < size; check++) {
-    checkCoord = coord;
-    checkCoord[0] = step[0] * check;
-    checkCoord[1] = step[1] * check;
-    if (checkCoord[0] > BOARDSIZE || checkCoord[0] < 0 ||
-      checkCoord[1] > BOARDSIZE || checkCoord[1] < 0 || board[checkCoord[0]][checkCoord[1]].ship) {
+  var checkCoord = coord.slice(0);
+  if (checkCoord[0] >= BOARDSIZE || checkCoord[0] < 0 ||
+    checkCoord[1] >= BOARDSIZE || checkCoord[1] < 0 || board[checkCoord[0]][checkCoord[1]].ship) {
+      return false
+  }
+  for (var check = 1; check < size; check++) {
+    // checkCoord = coord;
+    checkCoord[0] += step[0];
+    checkCoord[1] += step[1];
+    if (checkCoord[0] >= BOARDSIZE || checkCoord[0] < 0 ||
+      checkCoord[1] >= BOARDSIZE || checkCoord[1] < 0 || board[checkCoord[0]][checkCoord[1]].ship) {
         return false
     }
   }
@@ -79,11 +96,13 @@ var checkValidPlacement = function(board, coord, step, size) {
 var gameStart = function () {
   var player1 = {
     fleetBoard: setUpBoard(BOARDSIZE),
-    trackingBoard: setUpBoard(BOARDSIZE)
+    trackingBoard: setUpBoard(BOARDSIZE),
+    ships: []
   };
   var player2 = {
     fleetBoard: setUpBoard(BOARDSIZE),
-    trackingBoard: setUpBoard(BOARDSIZE)
+    trackingBoard: setUpBoard(BOARDSIZE),
+    ships: []
   };
 
   for (var idx = 0; idx < ships.length; idx++) {
