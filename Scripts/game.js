70fe24ed32ecdf0bb2d5d93e7affe398000c2d79
Angela Julie Dobbs <angela@jwplayer.com>
2016-07-18 16:51:47 -0400

    Begun working on ship placement and changed player to their own object

--- Scripts/game.js
+++ Scripts/game.js
@@ -2,6 +2,33 @@
 var BOARDSIZE = 10;
 var victory = false;
 var turn = 1;
+var ships = [
+  {
+    name: "Aircraft Carrier",
+    size: 5,
+    boardPositions: null
+  },
+  {
+    name: "Battleship",
+    size: 4,
+    boardPositions: null
+  },
+  {
+    name: "Destroyer",
+    size: 3,
+    boardPositions: null
+  },
+  {
+    name: "Submarine",
+    size: 3,
+    boardPositions: null
+  },
+  {
+    name: "Patrol Boat",
+    size: 2,
+    boardPositions: null
+  },
+]
 
 var setUpBoard = function(size) {
   var board = [], row;
@@ -12,19 +39,35 @@ var setUpBoard = function(size) {
      }
     board.push(row);
   }
-  board = placeShips(board);
   return board;
 };
 
-var placeShips = function(board) {
-  
+var placeShip = function(player, ship) {
+  var shipPlaced = false;
+  var directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+  var testCoord, testDir;
+  while (!shipPlaced) {
+    testCoord = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    testDir = Math.floor(Math.random() * 4);
+    
+  }
 };
 
 var gameStart = function () {
-  var player1Ships = setUpBoard(BOARDSIZE);
-  var player2Ships = setUpBoard(BOARDSIZE);
-  var player1Tracking = setUpBoard(BOARDSIZE);
-  var player2Tracking = setUpBoard(BOARDSIZE);
+  var player1 = {
+    fleetBoard: setUpBoard(BOARDSIZE),
+    trackingBoard = setUpBoard(BOARDSIZE)
+  };
+  var player2 = {
+    fleetBoard: setUpBoard(BOARDSIZE),
+    trackingBoard = setUpBoard(BOARDSIZE)
+  };
+
+  for (var idx = 0; idx < ships.length; idx++) {
+    placeShip(player1, ships[idx]);
+    placeShip(player2, ships[idx]);
+  }
+
 
   while (!victory) {
 
